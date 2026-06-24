3332439042702e64c02b57d38cf6ebd6798aa6fb
Monu Yadav <monuy7883@gmail.com>
2026-06-24 12:00:09 +0530

    production api urls

--- frontend/src/socket/socket.ts
+++ frontend/src/socket/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 
 const socket = io(
-  "http://localhost:5000",
+  "https://one-to-one-chat-applications-1.onrender.com",
   {
     transports: ["websocket"],
   }
